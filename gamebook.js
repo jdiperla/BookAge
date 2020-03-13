69fa4816d9cae45340aef16bf102830917cc0615
jdiperla <jdiperla@gmail.com>
2020-03-13 10:35:26 -0400

    Create gamebook.js
    Game data

---
+++ gamebook.js
@@ -0,0 +1,20 @@
+var GameObjects = {
+    'titlescreen': [
+      ['RoomName', 'Title Screen'],
+      ['RoomDesc', `Good afternoon. You have been selected to accomplish a mission. Pick your designation:<br><objdc vars="designation:007" id="room3">James Bond 007</objdc><br><objdc vars="designation:Max" id="room2">Mad Max</objdc>`],
+      ['XRes', '320'],
+      ['YRes', '240'],
+      ['RmImg', 'http://selmiak.bplaced.net/games/c64/zak/room/74_intro_00_256.png'],
+      ['RmMov','']
+      
+    ],
+    'room2': [
+      ['RoomName', 'Title Screen'],
+      ['RoomDesc', `It is a pleasure to meet you @-designation-@. You will begin your mission shortly. Please return to your office and try another designation.  Go back to the <objdc id="titlescreen">office</objdc>.`],
+      ['XRes', '320'],
+      ['YRes', '240'],
+      ['RmImg', 'http://selmiak.bplaced.net/games/c64/zak/room/74_intro_00_256.png'],
+      ['RmMov', '']
+      ]
+}
+
