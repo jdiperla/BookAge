d059d9079010be6658ecb9b1b6648586b3a47475
jdiperla <jdiperla@gmail.com>
2020-03-19 18:58:09 -0400

    Update gamebook.js
    Quick Commit

--- gamebook.js
+++ gamebook.js
@@ -1,7 +1,9 @@
+//This is an example gamebook file
+
 var GameObjects = {
     'titlescreen': [
       ['RoomName', 'Title Screen'],
-      ['RoomDesc', `Good afternoon. You have been selected to accomplish a mission. Pick your designation:<br><objdc vars="designation:007" id="room3">James Bond 007</objdc><br><objdc vars="designation:Max" id="room2">Mad Max</objdc>`],
+      ['RoomDesc', `Good afternoon. You have been selected to accomplish a mission. Pick your designation:<br><objdc vars="designation:007" id="design007" next="room2">James Bond 007</objdc><br><objdc id="designmax" vars="designation:Max" next="room2">Mad Max</objdc>`],
       ['XRes', '320'],
       ['YRes', '240'],
       ['RmImg', 'http://selmiak.bplaced.net/games/c64/zak/room/74_intro_00_256.png'],
@@ -10,7 +12,7 @@ var GameObjects = {
     ],
     'room2': [
       ['RoomName', 'Title Screen'],
-      ['RoomDesc', `It is a pleasure to meet you @-designation-@. You will begin your mission shortly. Please return to your office and try another designation.  Go back to the <objdc id="titlescreen">office</objdc>.`],
+      ['RoomDesc', `It is a pleasure to meet you @-designation-@. You will begin your mission shortly. Please return to your office and try another designation.  Go back to the <objdc id="goTitle" next="titlescreen">office</objdc>.`],
       ['XRes', '320'],
       ['YRes', '240'],
       ['RmImg', 'http://selmiak.bplaced.net/games/c64/zak/room/74_intro_00_256.png'],
